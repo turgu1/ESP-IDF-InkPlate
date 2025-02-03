a670c66258d3d24a1f97007e9398d2191b451b0a
Guy Turcotte <turgu666@gmail.com>
2025-02-03 08:56:50 -0500

    Some adjustments in support of INKPLATE_6

--- src/drivers/eink_6.hpp
+++ src/drivers/eink_6.hpp
@@ -2,7 +2,7 @@
 eink_6.hpp
 Inkplate 6 ESP-IDF
 
-Modified by Guy Turcotte 
+Modified by Guy Turcotte
 July 20, 2024
 
 from the Arduino Library:
@@ -14,9 +14,9 @@ https://github.com/e-radionicacom/Inkplate-6-Arduino-library
 For support, please reach over forums: forum.e-radionica.com/en
 For more info about the product, please check: www.inkplate.io
 
-This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
-Please review the LICENSE file included with this example.
-If you have any questions about licensing, please contact techsupport@e-radionica.com
+This code is released under the GNU Lesser General Public License v3.0:
+https://www.gnu.org/licenses/lgpl-3.0.en.html Please review the LICENSE file included with this
+example. If you have any questions about licensing, please contact techsupport@e-radionica.com
 Distributed as-is; no warranty is given.
 */
 
@@ -27,92 +27,93 @@ Distributed as-is; no warranty is given.
 #include <cinttypes>
 #include <cstring>
 
-#include "non_copyable.hpp"
 #include "driver/gpio.h"
 #include "eink.hpp"
+#include "non_copyable.hpp"
 
 #if PCAL6416
-  #include "pcal6416.hpp"
+#include "pcal6416.hpp"
 #else
-  #include "mcp23017.hpp"
+#include "mcp23017.hpp"
 #endif
 
 /**
  * @brief Low level e-Ink display
- * 
+ *
  * This class implements the low level methods required to control
  * and access the e-ink display of the InkPlate-6 device.
- * 
- * This is a singleton. It cannot be instanciated elsewhere. It is not 
+ *
+ * This is a singleton. It cannot be instanciated elsewhere. It is not
  * instanciated in the heap. This is reinforced by the C++ construction
  * below. It also cannot be copied through the NonCopyable derivation.
  */
 
-class EInk6 : public EInk, NonCopyable
-{
-  public:
-    EInk6(IOExpander & io_expander) : EInk(io_expander)
-      { }
+class EInk6 : public EInk, NonCopyable {
+public:
+  static const uint16_t WIDTH  = 800; // In pixels
+  static const uint16_t HEIGHT = 600; // In pixels
 
-    static const uint16_t WIDTH  = 800; // In pixels
-    static const uint16_t HEIGHT = 600; // In pixels
-    static const uint16_t BITMAP_SIZE_1BIT = (WIDTH * HEIGHT) >> 3;            // In bytes
-    static const uint32_t BITMAP_SIZE_3BIT = ((uint32_t) WIDTH * HEIGHT) >> 1; // In bytes
-    static const uint16_t LINE_SIZE_1BIT   = WIDTH >> 3;                       // In bytes
-    static const uint16_t LINE_SIZE_3BIT   = WIDTH >> 1;                       // In bytes
+  EInk6(IOExpander &io_expander) : EInk(io_expander, WIDTH) {}
 
-    inline int16_t  get_width() { return WIDTH;  }
-    inline int16_t get_height() { return HEIGHT; }
+  static const uint16_t BITMAP_SIZE_1BIT = (WIDTH * HEIGHT) >> 3;           // In bytes
+  static const uint32_t BITMAP_SIZE_3BIT = ((uint32_t)WIDTH * HEIGHT) >> 1; // In bytes
+  static const uint16_t LINE_SIZE_1BIT   = WIDTH >> 3;                      // In bytes
+  static const uint16_t LINE_SIZE_3BIT   = WIDTH >> 1;                      // In bytes
 
-    inline PanelState get_panel_state() { return panel_state; }
-    inline bool        is_initialized() { return initialized; }
+  inline int16_t get_width() { return WIDTH; }
+  inline int16_t get_height() { return HEIGHT; }
 
-    virtual inline FrameBuffer1Bit * new_frame_buffer_1bit() { return new FrameBuffer1BitX; }
-    virtual inline FrameBuffer3Bit * new_frame_buffer_3bit() { return new FrameBuffer3BitX; }
+  inline PanelState get_panel_state() { return panel_state; }
+  inline bool is_initialized() { return initialized; }
 
-    // All the following methods are protecting the I2C device trough
-    // the Wire::enter() and Wire::leave() methods. These are implementing a
-    // Mutex semaphore access control.
-    //
-    // If you ever add public methods, you *MUST* consider adding calls to Wire::enter()
-    // and Wire::leave() and insure no deadlock will happen... or modifu the mutex to use
-    // a recursive mutex.
+  virtual inline FrameBuffer1Bit *new_frame_buffer_1bit() { return new FrameBuffer1BitX; }
+  virtual inline FrameBuffer3Bit *new_frame_buffer_3bit() { return new FrameBuffer3BitX; }
 
-    bool setup();
+  // All the following methods are protecting the I2C device trough
+  // the Wire::enter() and Wire::leave() methods. These are implementing a
+  // Mutex semaphore access control.
+  //
+  // If you ever add public methods, you *MUST* consider adding calls to Wire::enter()
+  // and Wire::leave() and insure no deadlock will happen... or modifu the mutex to use
+  // a recursive mutex.
 
-    void update(FrameBuffer1Bit & frame_buffer);
-    void update(FrameBuffer3Bit & frame_buffer);
+  bool setup();
 
-    void partial_update(FrameBuffer1Bit & frame_buffer, bool force = false);
+  void update(FrameBuffer1Bit &frame_buffer);
+  void update(FrameBuffer3Bit &frame_buffer);
 
+  void partial_update(FrameBuffer1Bit &frame_buffer, bool force = false);
+
+private:
+  static constexpr char const *TAG = "EInk6";
+
+  class FrameBuffer1BitX : public FrameBuffer1Bit {
   private:
-    static constexpr char const * TAG = "EInk6";
+    uint8_t data[BITMAP_SIZE_1BIT];
 
-    class FrameBuffer1BitX : public FrameBuffer1Bit {
-      private:
-        uint8_t data[BITMAP_SIZE_1BIT];
-      public:
-        FrameBuffer1BitX() : FrameBuffer1Bit(WIDTH, HEIGHT, BITMAP_SIZE_1BIT) {}
-       
-        uint8_t * get_data() { return data; }
-    };
+  public:
+    FrameBuffer1BitX() : FrameBuffer1Bit(WIDTH, HEIGHT, BITMAP_SIZE_1BIT) {}
 
-    class FrameBuffer3BitX : public FrameBuffer3Bit {
-      private:
-        uint8_t data[BITMAP_SIZE_3BIT];
-      public:
-        FrameBuffer3BitX() : FrameBuffer3Bit(WIDTH, HEIGHT, BITMAP_SIZE_3BIT) {}
+    uint8_t *get_data() { return data; }
+  };
 
-        uint8_t * get_data() { return data; }
-    };
+  class FrameBuffer3BitX : public FrameBuffer3Bit {
+  private:
+    uint8_t data[BITMAP_SIZE_3BIT];
 
-    void clean(PixelState pixel_state, uint8_t repeat_count);
+  public:
+    FrameBuffer3BitX() : FrameBuffer3Bit(WIDTH, HEIGHT, BITMAP_SIZE_3BIT) {}
 
-    static const uint8_t  WAVEFORM_3BIT[8][8]; 
-    static const uint32_t WAVEFORM[50]; 
-    static const uint8_t  LUT2[16];
-    static const uint8_t  LUTW[16];
-    static const uint8_t  LUTB[16];
+    uint8_t *get_data() { return data; }
+  };
+
+  void clean(PixelState pixel_state, uint8_t repeat_count);
+
+  static const uint8_t WAVEFORM_3BIT[8][8];
+  static const uint32_t WAVEFORM[50];
+  static const uint8_t LUT2[16];
+  static const uint8_t LUTW[16];
+  static const uint8_t LUTB[16];
 };
 
 #endif
--- src/drivers/mcp23017.cpp
+++ src/drivers/mcp23017.cpp
@@ -2,7 +2,7 @@
 mcp23017.cpp
 Inkplate ESP-IDF
 
-Modified by Guy Turcotte 
+Modified by Guy Turcotte
 July 20, 2024
 
 from the Arduino Library:
@@ -14,9 +14,9 @@ https://github.com/e-radionicacom/Inkplate-6-Arduino-library
 For support, please reach over forums: forum.e-radionica.com/en
 For more info about the product, please check: www.inkplate.io
 
-This code is released under the GNU Lesser General Public License v3.0: https://www.gnu.org/licenses/lgpl-3.0.en.html
-Please review the LICENSE file included with this example.
-If you have any questions about licensing, please contact techsupport@e-radionica.com
+This code is released under the GNU Lesser General Public License v3.0:
+https://www.gnu.org/licenses/lgpl-3.0.en.html Please review the LICENSE file included with this
+example. If you have any questions about licensing, please contact techsupport@e-radionica.com
 Distributed as-is; no warranty is given.
 */
 
@@ -29,16 +29,12 @@ Distributed as-is; no warranty is given.
 
 // LOW LEVEL:
 
-bool
-IOExpander::check_presence()
-{
+bool IOExpander::check_presence() {
   if (!present) ESP_LOGE(TAG, "The MCP at address 0x%X has not been detected.", mcp_address);
   return present;
 }
 
-void 
-IOExpander::test()
-{
+void IOExpander::test() {
   if (!check_presence()) return;
 
   printf("Registers before read:\n");
@@ -58,14 +54,12 @@ IOExpander::test()
   fflush(stdout);
 }
 
-bool 
-IOExpander::setup()
-{
+bool IOExpander::setup() {
   ESP_LOGD(TAG, "Initializing...");
-  
+
   wire_device = new WireDevice(mcp_address);
-  present = (wire_device != nullptr) && wire_device->is_initialized();
-  
+  present     = (wire_device != nullptr) && wire_device->is_initialized();
+
   ESP_LOGI(TAG, "MCP at address 0x%X has%s been detected", mcp_address, present ? "" : " NOT");
 
   if (present) {
@@ -78,13 +72,11 @@ IOExpander::setup()
   return present;
 }
 
-void 
-IOExpander::read_all_registers()
-{
+void IOExpander::read_all_registers() {
   if (!check_presence()) return;
 
   wire_device->cmd_read(0x00, registers.data(), registers.size());
-  
+
   // wire.begin_transmission(mcp_address);
   // wire.write(0x00);
   // wire.end_transmission();
@@ -94,17 +86,13 @@ IOExpander::read_all_registers()
   // }
 }
 
-void 
-IOExpander::read_registers(Reg first_reg, uint8_t count)
-{
+void IOExpander::read_registers(Reg first_reg, uint8_t count) {
   if (!check_presence()) return;
 
   wire_device->cmd_read(static_cast<int8_t>(first_reg), &registers[first_reg], count);
 }
 
-uint8_t 
-IOExpander::read_register(Reg reg)
-{
+uint8_t IOExpander::read_register(Reg reg) {
   if (!check_presence()) return 0;
 
   registers[reg] = wire_device->cmd_read(static_cast<int8_t>(reg));
@@ -118,9 +106,7 @@ IOExpander::read_register(Reg reg)
   return registers[reg];
 }
 
-void 
-IOExpander::update_all_registers()
-{
+void IOExpander::update_all_registers() {
   if (!check_presence()) return;
 
   wire_device->cmd_write(0x00, registers.data(), registers.size());
@@ -133,9 +119,7 @@ IOExpander::update_all_registers()
   // wire.end_transmission();
 }
 
-void 
-IOExpander::update_register(Reg reg, uint8_t value)
-{
+void IOExpander::update_register(Reg reg, uint8_t value) {
   if (!check_presence()) return;
 
   wire_device->cmd_write(static_cast<uint8_t>(reg), value);
@@ -146,9 +130,7 @@ IOExpander::update_register(Reg reg, uint8_t value)
   // wire.end_transmission();
 }
 
-void 
-IOExpander::update_registers(Reg first_reg, uint8_t count)
-{
+void IOExpander::update_registers(Reg first_reg, uint8_t count) {
   if (!check_presence()) return;
 
   wire_device->cmd_write(static_cast<uint8_t>(first_reg), &registers[first_reg], count);
@@ -163,131 +145,112 @@ IOExpander::update_registers(Reg first_reg, uint8_t count)
 
 // HIGH LEVEL:
 
-void
-IOExpander::set_direction(Pin pin, PinMode mode)
-{
-  uint8_t port = (static_cast<uint8_t>(pin >> 3)) & 1;
-  uint8_t p    =  static_cast<uint8_t>(pin & 7);
+void IOExpander::set_direction(Pin pin, PinMode mode) {
+  uint8_t port = (static_cast<uint8_t>(pin) >> 3) & 1;
+  uint8_t p    = static_cast<uint8_t>(pin) & 7;
 
   if (!check_presence()) return;
 
   switch (mode) {
-    case PinMode::INPUT:
-      registers[R(Reg::IODIRA, port)] |=   1 << p;  // Set it to input
-      registers[R(Reg::GPPUA,  port)] &= ~(1 << p); // Disable pullup on that pin
-      update_register(R(Reg::IODIRA, port), registers[R(Reg::IODIRA, port)]);
-      update_register(R(Reg::GPPUA,  port), registers[R(Reg::GPPUA,  port)]);
-      break;
+  case PinMode::INPUT:
+    registers[R(Reg::IODIRA, port)] |= 1 << p;   // Set it to input
+    registers[R(Reg::GPPUA, port)] &= ~(1 << p); // Disable pullup on that pin
+    update_register(R(Reg::IODIRA, port), registers[R(Reg::IODIRA, port)]);
+    update_register(R(Reg::GPPUA, port), registers[R(Reg::GPPUA, port)]);
+    break;
 
   case PinMode::INPUT_PULLUP:
-      registers[R(Reg::IODIRA, port)] |= 1 << p; // Set it to input
-      registers[R(Reg::GPPUA,  port)] |= 1 << p; // Enable pullup on that pin
-      update_register(R(Reg::IODIRA, port), registers[R(Reg::IODIRA, port)]);
-      update_register(R(Reg::GPPUA,  port), registers[R(Reg::GPPUA,  port)]);
-      break;
+    registers[R(Reg::IODIRA, port)] |= 1 << p; // Set it to input
+    registers[R(Reg::GPPUA, port)] |= 1 << p;  // Enable pullup on that pin
+    update_register(R(Reg::IODIRA, port), registers[R(Reg::IODIRA, port)]);
+    update_register(R(Reg::GPPUA, port), registers[R(Reg::GPPUA, port)]);
+    break;
 
   case PinMode::OUTPUT:
-      registers[R(Reg::IODIRA, port)] &= ~(1 << p); // Set it to output
-      registers[R(Reg::GPPUA,  port)] &= ~(1 << p); // Disable pullup on that pin
-      update_register(R(Reg::IODIRA, port), registers[R(Reg::IODIRA, port)]);
-      update_register(R(Reg::GPPUA,  port), registers[R(Reg::GPPUA,  port)]);
-      break;
+    registers[R(Reg::IODIRA, port)] &= ~(1 << p); // Set it to output
+    registers[R(Reg::GPPUA, port)] &= ~(1 << p);  // Disable pullup on that pin
+    update_register(R(Reg::IODIRA, port), registers[R(Reg::IODIRA, port)]);
+    update_register(R(Reg::GPPUA, port), registers[R(Reg::GPPUA, port)]);
+    break;
   }
 }
 
-void 
-IOExpander::digital_write(Pin pin, SignalLevel state)
-{
-  uint8_t port = (static_cast<uint8_t>(pin >> 3)) & 1;
-  uint8_t p    =  static_cast<uint8_t>(pin & 7);
+void IOExpander::digital_write(Pin pin, SignalLevel state) {
+  uint8_t port = (static_cast<uint8_t>(pin) >> 3) & 1;
+  uint8_t p    = static_cast<uint8_t>(pin) & 7;
 
   if (registers[R(Reg::IODIRA, port)] & (1 << p)) return;
-  state == SignalLevel::HIGH ? (registers[R(Reg::GPIOA, port)] |=  (1 << p)) : 
-          (registers[R(Reg::GPIOA, port)] &= ~(1 << p));
+  state == SignalLevel::HIGH ? (registers[R(Reg::GPIOA, port)] |= (1 << p))
+                             : (registers[R(Reg::GPIOA, port)] &= ~(1 << p));
   update_register(R(Reg::GPIOA, port), registers[R(Reg::GPIOA, port)]);
 }
 
-IOExpander::SignalLevel 
-IOExpander::digital_read(Pin pin)
-{
-  uint8_t port = (static_cast<uint8_t>(pin >> 3)) & 1;
-  uint8_t p    =  static_cast<uint8_t>(pin & 7);
-  uint8_t r = read_register((Reg)(static_cast<int8_t>(Reg::GPIOA) + port));
+IOExpander::SignalLevel IOExpander::digital_read(Pin pin) {
+  uint8_t port = (static_cast<uint8_t>(pin) >> 3) & 1;
+  uint8_t p    = static_cast<uint8_t>(pin) & 7;
+  uint8_t r    = read_register((Reg)(static_cast<int8_t>(Reg::GPIOA) + port));
 
   return (r & (1 << p)) ? SignalLevel::HIGH : SignalLevel::LOW;
 }
 
-void 
-IOExpander::set_int_output(IntPort intPort, bool mirroring, bool openDrain, SignalLevel polarity)
-{
+void IOExpander::set_int_output(IntPort intPort, bool mirroring, bool openDrain,
+                                SignalLevel polarity) {
   Reg reg = intPort == IntPort::INTPORTA ? Reg::IOCONA : Reg::IOCONB;
 
   registers[reg] = (registers[reg] & ~(1 << 6)) | (mirroring << 6);
   registers[reg] = (registers[reg] & ~(1 << 2)) | (openDrain << 2);
   registers[reg] = (registers[reg] & ~(1 << 1)) | (polarity == SignalLevel::HIGH ? (1 << 1) : 0);
-  
+
   update_register(reg, registers[reg]);
 }
 
-void 
-IOExpander::set_int_pin(Pin pin, IntMode mode)
-{
-  uint8_t port = (static_cast<uint8_t>(pin >> 3)) & 1;
-  uint8_t p    =  static_cast<uint8_t>(pin & 7);
+void IOExpander::set_int_pin(Pin pin, IntMode mode) {
+  uint8_t port = (static_cast<uint8_t>(pin) >> 3) & 1;
+  uint8_t p    = static_cast<uint8_t>(pin) & 7;
 
   switch (mode) {
-    case IntMode::CHANGE:
-      registers[R(Reg::INTCONA, port)] &= ~(1 << p);
-      break;
+  case IntMode::CHANGE:
+    registers[R(Reg::INTCONA, port)] &= ~(1 << p);
+    break;
 
-    case IntMode::FALLING:
-      registers[R(Reg::INTCONA, port)] |= (1 << p);
-      registers[R(Reg::DEFVALA, port)] |= (1 << p);
-      break;
+  case IntMode::FALLING:
+    registers[R(Reg::INTCONA, port)] |= (1 << p);
+    registers[R(Reg::DEFVALA, port)] |= (1 << p);
+    break;
 
-    case IntMode::RISING:
-      registers[R(Reg::INTCONA, port)] |=  (1 << p);
-      registers[R(Reg::DEFVALA, port)] &= ~(1 << p);
-      break;
+  case IntMode::RISING:
+    registers[R(Reg::INTCONA, port)] |= (1 << p);
+    registers[R(Reg::DEFVALA, port)] &= ~(1 << p);
+    break;
   }
   registers[R(Reg::GPINTENA, port)] |= (1 << p);
   update_registers(Reg::GPINTENA, 6);
 }
 
-void 
-IOExpander::remove_int_pin(Pin pin)
-{
-  uint8_t port = (static_cast<uint8_t>(pin >> 3)) & 1;
-  uint8_t p    =  static_cast<uint8_t>(pin & 7);
+void IOExpander::remove_int_pin(Pin pin) {
+  uint8_t port = (static_cast<uint8_t>(pin) >> 3) & 1;
+  uint8_t p    = static_cast<uint8_t>(pin) & 7;
   registers[R(Reg::GPINTENA, port)] &= ~(1 << p);
   update_registers(Reg::GPINTENA, 2);
 }
 
-uint16_t 
-IOExpander::get_int()
-{
+uint16_t IOExpander::get_int() {
   read_registers(Reg::INTFA, 2);
   return ((registers[Reg::INTFB] << 8) | registers[Reg::INTFA]);
 }
 
-uint16_t 
-IOExpander::get_int_state()
-{
+uint16_t IOExpander::get_int_state() {
   read_registers(Reg::INTCAPA, 2);
   return ((registers[Reg::INTCAPB] << 8) | registers[Reg::INTCAPA]);
 }
 
-void 
-IOExpander::set_ports(uint16_t values)
-{
+void IOExpander::set_ports(uint16_t values) {
   registers[Reg::GPIOA] = values & 0xff;
   registers[Reg::GPIOB] = (values >> 8) & 0xff;
   update_registers(Reg::GPIOA, 2);
 }
 
-uint16_t 
-IOExpander::get_ports()
-{
+uint16_t IOExpander::get_ports() {
   read_registers(Reg::GPIOA, 2);
   return ((registers[Reg::GPIOB] << 8) | (registers[Reg::GPIOA]));
 }
