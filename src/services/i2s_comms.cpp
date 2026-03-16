924dc17a521db07d1cd7d0c67109948cba0949b2
Guy Turcotte <turgu666@gmail.com>
2026-03-16 11:30:42 -0400

    Removed redundant IRAM_ATTR

--- src/services/i2s_comms.cpp
+++ src/services/i2s_comms.cpp
@@ -50,7 +50,7 @@
  *
  * @note        Function must be declared static to fit into Instruction RAM of the ESP32.
  */
-void IRAM_ATTR my_I2SInit(i2s_dev_t *_i2sDev, uint8_t _clockDivider) {
+void my_I2SInit(i2s_dev_t *_i2sDev, uint8_t _clockDivider) {
   // Enable I2S peripheral and reset it.
   periph_module_enable(PERIPH_I2S1_MODULE);
   periph_module_reset(PERIPH_I2S1_MODULE);
@@ -131,7 +131,7 @@ void IRAM_ATTR my_I2SInit(i2s_dev_t *_i2sDev, uint8_t _clockDivider) {
  * @note        Function must be declared static to fit into Instruction RAM of the ESP32. Also, DMA
  * descriptor must be already configured!
  */
-void IRAM_ATTR my_sendDataI2S(i2s_dev_t *_i2sDev, volatile lldesc_s *_dmaDecs) {
+void my_sendDataI2S(i2s_dev_t *_i2sDev, volatile lldesc_s *_dmaDecs) {
   // Stop any on-going transmission (just in case).
   _i2sDev->out_link.stop  = 1;
   _i2sDev->out_link.start = 0;
@@ -180,7 +180,7 @@ void IRAM_ATTR my_sendDataI2S(i2s_dev_t *_i2sDev, volatile lldesc_s *_dmaDecs) {
   _i2sDev->out_link.start = 0;
 }
 
-void IRAM_ATTR my_setI2S1pin(uint32_t _pin, uint32_t _function, uint32_t _inv) {
+void my_setI2S1pin(uint32_t _pin, uint32_t _function, uint32_t _inv) {
   // Check if valid pin is selected
   if (_pin > 39) return;
 
