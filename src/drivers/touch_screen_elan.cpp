546762070ae5ac87922f1db4edfcb33c3ac75a36
Guy Turcotte <turgu666@gmail.com>
2026-03-23 16:23:37 -0400

    Corrected ELan Pullup / Pulldown mgt on interrupt pin.

--- src/drivers/touch_screen_elan.cpp
+++ src/drivers/touch_screen_elan.cpp
@@ -38,7 +38,8 @@
     io_conf.intr_type    = GPIO_INTR_NEGEDGE;
     io_conf.pin_bit_mask = 1ULL << INTERRUPT_PIN;
     io_conf.mode         = GPIO_MODE_INPUT;
-    io_conf.pull_up_en   = GPIO_PULLUP_ENABLE;
+    io_conf.pull_up_en   = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
 
     gpio_config(&io_conf);
 
